f490229f4f68358ca1bd755be95a66a2c52f6aa2
Rafael Speranza de Souza <r.speranza@uni9.edu.br>
2025-10-07 00:24:43 +0000

    feat:tarefa

---
+++ tarefas.cs
@@ -0,0 +1,14 @@
+public class Tarefa
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Descricao { get; set; }
+
+    public dataTime dataCriacao { get; set; }
+
+    public int status { get; set; }
+
+    public dataTime dataExecucao { get; set; }
+}
