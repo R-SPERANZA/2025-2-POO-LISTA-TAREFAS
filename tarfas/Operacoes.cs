7a1fe678f8d0baf81bab0fa36980b0ea77672ee9
Rafael Speranza de Souza <r.speranza@uni9.edu.br>
2025-10-24 23:37:56 +0000

    tarefas

--- tarfas/Operacoes.cs
+++ tarfas/Operacoes.cs
@@ -1,12 +1,15 @@
 using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
 
 public class Operacoes
 {
-    private string connectionString = 
+    private string connectionString =
     @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
+
     public int Criar(Tarefa tarefa)
     {
-        using(var conexao = new MySqlConnection(connectionString))
+        using (var conexao = new MySqlConnection(connectionString))
         {
             conexao.Open();
             string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao) 
@@ -18,7 +21,7 @@ public class Operacoes
                 cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
                 cmd.Parameters.AddWithValue("@dataCriacao", tarefa.DataCriacao);
                 cmd.Parameters.AddWithValue("@status", tarefa.Status);
-                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
+                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao ?? (object)DBNull.Value);
 
                 return Convert.ToInt32(cmd.ExecuteScalar());
             }
@@ -33,44 +36,84 @@ public class Operacoes
     public IList<Tarefa> Listar()
     {
         var tarefas = new List<Tarefa>();
-        using(var conexao = new MySqlConnection(connectionString))
-        {            
-            var sql = "SELECT id, nome, descricao, dataCriacao, dataExecucao, status FROM `tarefa`";
-            conexao.Open();
+
+        using (var conexao = new MySqlConnection(connectionString))
+        {
+            // 🔧 Corrigido: remover aspas simples do nome da tabela e usar aspas corretas
+            var sql = "SELECT id, nome, descricao, dataCriacao, dataExecucao, status FROM tarefa";
 
             using (var cmd = new MySqlCommand(sql, conexao))
-            using (var reader = cmd.ExecuteReader())
             {
-                while (reader.Read())
+                conexao.Open();
+
+                // 🔧 Corrigido: nome do método correto é ExecuteReader (maiúsculo)
+                using (var reader = cmd.ExecuteReader())
                 {
-                    var tarefa = new Tarefa
+                    while (reader.Read())
                     {
-                        Id = reader.GetInt32("id"),
-                        Nome = reader.GetString("nome"),
-                        Descricao = reader.GetString("descricao"),
-                        DataCriacao = reader.GetDateTime("dataCriacao"),
-                        DataExecucao = reader.IsDBNull(reader.GetOrdinal("dataExecucao"))
-                                       ? (DateTime?)null
-                                       : reader.GetDateTime("dataExecucao"),
-                        Status = reader.GetInt32("status")
-                    };
+                        var tarefa = new Tarefa
+                        {
+                            Id = reader.GetInt32("id"),
 
-                    tarefas.Add(tarefa);
-                     
-                }               
+                            // 🔧 Corrigido: métodos GetString, não GetString32
+                            Nome = reader.GetString("nome"),
+                            Descricao = reader.GetString("descricao"),
+
+                            // 🔧 Corrigido: método GetDateTime, não GetDateTime32
+                            DataCriacao = reader.GetDateTime("dataCriacao"),
+
+                            DataExecucao = reader.IsDBNull(reader.GetOrdinal("dataExecucao"))
+                                ? (DateTime?)null
+                                : reader.GetDateTime("dataExecucao"),
+
+                            Status = reader.GetInt32("status")
+                        };
+
+                        tarefas.Add(tarefa);
+                    }
+                }
             }
-
         }
+
+        // 🔧 Corrigido: deve retornar a lista preenchida, não Array.Empty<Tarefa>()
         return tarefas;
     }
 
     public void Alterar(Tarefa tarefa)
     {
+        string sql = @"UPDATE tarefa SET 
+                           nome = @nome, 
+                           descricao = @descricao, 
+                           status = @status, 
+                           dataExecucao = @dataExecucao
+                       WHERE id = @id;";
 
+        using (var conexao = new MySqlConnection(connectionString))
+        {
+            conexao.Open();
+            using (var cmd = new MySqlCommand(sql, conexao))
+            {
+                cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+                cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+                cmd.Parameters.AddWithValue("@status", tarefa.Status);
+                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@id", tarefa.Id);
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 
     public void Excluir(int id)
     {
-
+        using (var conexao = new MySqlConnection(connectionString))
+        {
+            conexao.Open();
+            string sql = "DELETE FROM tarefa WHERE id = @id";
+            using (var cmd = new MySqlCommand(sql, conexao))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
